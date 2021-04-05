11d7d4876b7546527f987b2aa09cee04dfce85f7
julianog <>
2021-04-05 02:20:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+#Día actual
+today = date.today()
+
+
+print(today)
+print("El día actual es {}".format(today.day))
+print("El mes actual es {}".format(today.month))
+print("El año actual es {}".format(today.year))
+new_date = datetime(2019, 2, 28)
